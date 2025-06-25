5b0fdf25d9105861a5d1bbe38521000eb6b76dd2
sso9594 <sso9594@naver.com>
2025-06-26 02:24:56 +0900

    feat: 문제 선다 도메인 코어

---
+++ haruhanip-domain/domain-rds/src/main/java/com/clubnu/domains/problem/repository/ProblemOptionRepository.java
@@ -0,0 +1,7 @@
+package com.clubnu.domains.problem.repository;
+
+import com.clubnu.domains.problem.domain.ProblemOption;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProblemOptionRepository extends JpaRepository<ProblemOption, Long> {
+}
