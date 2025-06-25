eb5de90001b8838e37d2c1c792ef30915f3e4ac5
sso9594 <sso9594@naver.com>
2025-06-26 02:33:45 +0900

    feat: Daily 명령 Api

---
+++ haruhanip-exteranl-api/src/main/java/com/clubnu/api/daily/service/DailyCommandService.java
@@ -0,0 +1,57 @@
+package com.clubnu.api.daily.service;
+
+import com.clubnu.api.daily.dto.DailyCreateRequest;
+import com.clubnu.domains.category.domain.Category;
+import com.clubnu.domains.category.repository.CategoryRepository;
+import com.clubnu.domains.daily.domain.Daily;
+import com.clubnu.domains.daily.repository.DailyRepository;
+import com.clubnu.domains.problem.domain.Problem;
+import com.clubnu.domains.problem.repository.ProblemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class DailyCommandService {
+
+    private final DailyRepository dailyRepository;
+    private final ProblemRepository problemRepository;
+    private final CategoryRepository categoryRepository;
+
+    @Transactional
+    public void createDaily(DailyCreateRequest request) {
+        Category category = categoryRepository
+                .findById(request.categoryId())
+                .orElseThrow(() -> new IllegalArgumentException("Category not found"));
+
+        Daily target = Daily.builder()
+                .dailyDate(request.dailyDate())
+                .category(category)
+                .build();
+
+        LocalDateTime start = request.dailyDate().atStartOfDay();
+        LocalDateTime end   = start.plusDays(1);
+        List<Problem> problems = problemRepository
+                .findAllByCategoryAndCreatedAtBetween(
+                        category, start, end
+                );
+
+        for (Problem problem : problems) {
+            target.addProblem(problem);
+        }
+
+        dailyRepository.save(target);
+    }
+
+    @Transactional
+    public void deleteDaily(Long dailyId) {
+        Daily daily = dailyRepository
+                .findById(dailyId)
+                .orElseThrow(() -> new IllegalArgumentException("Daily not found"));
+        dailyRepository.delete(daily);
+    }
+}
